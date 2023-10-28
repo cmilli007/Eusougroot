23b1c74257f5bac13f00c457a2c5e242bb22a6bd
camili costa <camili.costa07@gmail.com>
2023-10-27 22:39:41 -0300

    precisa terminar

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿const string ParaSairDigite = "tchau Groot";
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("--CHAT DO GROOT--");
+Console.ResetColor();
+string pergunta;
+
+do 
+{
+    Console.WriteLine("Faça uma pergunta para o Groot?");
+     pergunta = Console.ReadLine()!;
+     string resposta = GrootResponde(pergunta);
+
+            Exibe(GrootResponde(pergunta));
+}
+while (pergunta.Trim().ToLower() != ParaSairDigite);
+
+    void Exibe(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+    string resposta(string pergunta);
+{
+    return pergunta.Trim().ToLower() == FraseParaSair ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
