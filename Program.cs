fcbf56bb2234cb92b5d9341b27a7e29beeb0cb3a
camili <camili.costa07@gmail.com>
2023-10-30 20:38:39 -0300

    finalizar

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
-﻿const string ParaSairDigite = "tchau Groot";
+﻿const string ParaSairDigite = "tchau groot";
+
 Console.BackgroundColor = ConsoleColor.Green;
 Console.WriteLine("--CHAT DO GROOT--");
 Console.ResetColor();
+
 string pergunta;
 
 do 
 {
     Console.WriteLine("Faça uma pergunta para o Groot?");
+        Console.Write("Ou se quiser sair digite:"tchau groot"");
+            Console.WriteLine();
      pergunta = Console.ReadLine()!;
-     string resposta = GrootResponde(pergunta);
+ 
+ string resposta = GrootResponde(pergunta);
 
-            Exibe(GrootResponde(pergunta));
-}
-while (pergunta.Trim().ToLower() != ParaSairDigite);
+            ExibeResposta(GrootResponde(pergunta));
+}while (pergunta.Trim().ToLower() != ParaSairDigite);
 
-    void Exibe(string resposta)
+    void ExibeResposta(string resposta)
 {
     Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine($"Resposta: {resposta}");
+    Console.WriteLine($"            Resposta: {resposta}");
     Console.ResetColor();
 }
 
-    string resposta(string pergunta);
+ string GrootResponde( string pergunta)
+
 {
-    return pergunta.Trim().ToLower() == FraseParaSair ?
-        "Eu sou Groot!" :
-        "Eu sou Groot.";
+    return pergunta.Trim().ToLower() == ParaSairDigite ?
+       "Eu sou Groot!":
+       "Eu sou Groot!";
 }
